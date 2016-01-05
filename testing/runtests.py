d5639f66a9365c7154a0510f324f037ee2945d10
inetrica <inetrica@gmail.com>
2016-01-04 20:56:19 -0600

    python script to run & log tests

--- testing/runtests.py
+++ testing/runtests.py
@@ -1,11 +1,16 @@
 #!/usr/bin/env python3
 import datetime as dt
+import subprocess
 
 def main():
 
     fileName = dt.datetime.now().strftime("%Y%m%d_%H%M")
-    print("{}".format(fileName))
-    #logfile = open
+    print("{}.txt".format(fileName))
+    logfile = open("Log/{}.txt".format(fileName), 'w')
+    #subprocess.call(['./runtests'])
+    output = subprocess.check_output(['./runtests'])
+    logfile.write(output.decode("utf-8"))
+    logfile.close()
 
 if __name__ == "__main__":
     main()
