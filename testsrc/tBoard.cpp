337f9f1aa221eb39772aba9bf283790b1445caf5
inetrica <inetrica@gmail.com>
2016-01-07 03:44:41 -0600

    added to Board tests

--- testsrc/tBoard.cpp
+++ testsrc/tBoard.cpp
@@ -17,11 +17,28 @@ testBoardInit(){
 	std::cout << std::endl;
 }
 
+void
+testSetBlockOnBoard(){
+	Board board;
+	//check initially empty
+	assert("TestRow0Col1 Empty", board.getBlockValueAt(0,1) == Block::Opt_E);
+	
+	//set empty block to X, check block is X
+	assert("TestSetRow0Col1 To X", 0 == board.setBlock(0, 1, Block::Opt_X));
+	assert("TestRow0Col1 is X", board.getBlockValueAt(0, 1) == Block::Opt_X);
+
+	//try to set block from X to O, should fail with return value -2, should still be set to X
+	assert("TestSetRow0Col1 To O fails", -2 == board.setBlock(0, 1, Block::Opt_O));
+	assert("TestRow0Col1 still X", board.getBlockValueAt(0, 1) == Block::Opt_X);
+}
+
 
 
 int
 main(){
 	testBoardInit();
+
+	testSetBlockOnBoard();
 
 	return 0;
 
