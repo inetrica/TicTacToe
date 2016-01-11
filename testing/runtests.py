cd417aa3aafd5c0ec0e70711e796a864f962b7c0
inetrica <inetrica@gmail.com>
2016-01-11 03:31:42 -0600

    added to python script to automatically print out if anything failed

--- testing/runtests.py
+++ testing/runtests.py
@@ -2,6 +2,15 @@
 import datetime as dt
 import subprocess
 
+'''
+check if any of the tests failed by grepping the Logfile just created,
+searching for the phrase "Failed~"
+'''
+def grepLog(fileName):
+    output = subprocess.call(\
+            ["grep", "Failed~", "Log/{}.txt".format(fileName)])
+    #print(output)
+
 def main():
 
     fileName = dt.datetime.now().strftime("%Y%m%d_%H%M")
@@ -11,6 +20,8 @@ def main():
     output = subprocess.check_output(['./runtests'])
     logfile.write(output.decode("utf-8"))
     logfile.close()
+
+    grepLog(fileName)
 
 if __name__ == "__main__":
     main()
